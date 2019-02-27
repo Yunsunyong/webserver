3761c01ec00e09e453b8257a20c837983b069ad4
Yunsunyong <45868485+Yunsunyong@users.noreply.github.com>
2019-02-27 21:20:24 +0900

    d
    d

---
+++ 2019-02-27 webserver 두번째(session login,logout)/firstWebProject/src/member/model/service/MemberService.java
@@ -0,0 +1,21 @@
+package member.model.service;
+
+import member.model.dao.MemberDao;
+import member.model.vo.Member;
+
+import static common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+public class MemberService {
+	private MemberDao mdao = new MemberDao();
+	
+	public MemberService() {}
+	
+	public Member selectLogin(String userId, String userPwd) {
+		Connection conn = getConnection();
+		Member loginUser = mdao.selectLogin(userId, userPwd, conn);
+		close(conn);
+		return loginUser;
+	}
+}
